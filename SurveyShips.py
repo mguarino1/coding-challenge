488c36692e89600c333ef9ee124b39aa8eba7cc3
Mike <mguarino223@gmail.com>
2020-01-26 18:03:53 -0500

    Implemented invalid moves and added comments, complete

--- SurveyShips.py
+++ SurveyShips.py
@@ -1,22 +1,28 @@
 #Michael Guarino
 #StratumFive Coding Challenge
 
-#Open input file
+#Open input and output files
 rf = open("SampleInput.txt", "r")
-
-#Grabs first line to create the grid
+wf = open("Output.txt", "w")
+#Grabs first line to create grid bounds
 x, y = rf.readline().split(" ")
-grid = [['0' for col in range(int(x)+1)] for row in range(int(y)+1)]
+
+x = int(x)
+y = int(y)
 
 #Puts all ship positions and commands into list, removes line spacing
 ships = rf.readlines()
 for i in range(len(ships)-1):
     ships[i] = ships[i].strip()
 
+#Directions
 N=0
 E=1
 S=2
 W=3
+
+#Reference for moves that have been marked as invalid
+invalidMoves = []
 
 while ships:
     #Removing blank lines from list
@@ -36,38 +42,69 @@ while ships:
         shipDir = S
     elif shipDir is 'W':
         shipDir = W
-       
-    curCommands = ships.pop(0)
+ 
+    curCommands = ships.pop(0).strip()
     lost = False
     
     for c in curCommands:
-            if c is 'L':
-                shipDir = (shipDir+1)%4
-            elif c is 'R':
-                shipDir = (shipDir-1)%4
-            else:
+        #Turn left
+        if c is 'L':
+            shipDir = (shipDir-1)%4
+        #Turn right
+        elif c is 'R':
+            shipDir = (shipDir+1)%4
+        #Move forward
+        elif c is 'F':
+            #Ignores move if it's invalid
+            if [curX,curY,shipDir] not in invalidMoves:
+                #Moves ship in appropriate direction or marks the ship
+                #as lost and records the invalid move if ship goes
+                #off the edge
                 if shipDir == N:
-                    if curY == 3:
+                    if curY == y:
                         lost = True
+                        invalidMoves.append([curX,curY,shipDir])
                     else:
                         curY+=1          
                 elif shipDir == E:
-                    if curX == 5:
+                    if curX == x:
                         lost = True
+                        invalidMoves.append([curX,curY,shipDir])
                     else:
                         curX+=1
                 elif shipDir == S:
                     if curY == 0:
                         lost = True
+                        invalidMoves.append([curX,curY,shipDir])
                     else:
                         curY-=1
                 else:
                     if curX == 0:
                         lost = True
+                        invalidMoves.append([curX,curY,shipDir])
                     else:
                         curX-=1
+        #If a ship falls off the edge, ignore the rest of the commands                
+        if lost:
+            break
+    #Changes directions back to characters for output purposes
+    if shipDir == 0:
+        shipDir = 'N'
+    elif shipDir == 1:
+        shipDir = 'E'
+    elif shipDir == 2:
+        shipDir = 'S'
+    else:
+        shipDir = 'W'
+    #Writes the result of each ship to the file     
+    if lost:
+        print(curX,curY,shipDir,'LOST', file=wf)
+    else:
+        print(curX,curY,shipDir, file=wf)
 
+rf.close()
+wf.close()
     
 
-wf = open("Output.txt", "w")
+
 

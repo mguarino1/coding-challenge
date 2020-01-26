66f79354523442a47af9f5755a61121c1eae78ab
Mike <mguarino223@gmail.com>
2020-01-26 14:37:52 -0500

    Parsing data from input

--- SurveyShips.py
+++ SurveyShips.py
@@ -1,10 +1,30 @@
 #Michael Guarino
 #StratumFive Coding Challenge
 
+#Open input file
 rf = open("SampleInput.txt", "r")
-x, y = rf.readline().split(" ")
 
+#Grabs first line to create the grid
+x, y = rf.readline().split(" ")
 grid = [['0' for col in range(int(x))] for row in range(int(y))]
+
+#Puts all ship positions and commands into list, removes line spacing
+ships = rf.readlines()
+for i in range(len(ships)-1):
+    ships[i] = ships[i].strip()
+
+while ships:
+    #Removing blank lines from list
+    if ships[0] == '':
+        ships.pop(0)
+    #Assumes ship position is first followed by ship instructions
+    curShip = ships.pop(0).split(" ")
+    curShip[0] = int(curShip[0])
+    curShip[1] = int(curShip[1])
+    curCommands = ships.pop(0)
+
+
+
 
 wf = open("Output.txt", "w")
 
